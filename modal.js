036d20dc9e8922f3c660f4cf5c108203f2c06ee8
Aqdas <aqdasdar91@gmail.com>
2023-12-11 16:41:17 +0100

    fix

--- modal.js
+++ modal.js
@@ -1,4 +1,5 @@
-function editNav() {
+function editNav(event) {
+    event.preventDefault();
   let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -11,28 +12,32 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const closeBtn = document.querySelectorAll(".close");
-
+const NavResponsive = document.getElementById("navResponsive");
+const form = document.getElementById("form");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+NavResponsive.forEach((btn) => btn.addEventListener("click", editNav));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
 
-// close modal form
+// fermer modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
 
 //form
-document.getElementsByName("reserve")[0].addEventListener("submit", validate);
+form.addEventListener("submit", validate);
 function validate(event) {
 
 
+    ///Gestion des erreurs
     //first name
-     let firstName = event.target.first.value;
+     let firstName = event.target.first.value; //event.target.first.value = document.getElementById("first").value
+    // pour récupérer la valeur du champ first dans le formulaire reserve qui a été soumis (submit) et qui a été capturé par l'event
     let firstNameError = document.getElementById("firstError");
     if (firstName.length < 2) {
         firstNameError.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
@@ -54,7 +59,7 @@ function validate(event) {
     //email regex
     let email = document.getElementById("email").value;
     let emailError = document.getElementById("emailError");
-    let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    let emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; //regex email format
     if (!emailRegex.test(email)) {
         emailError.innerHTML = "Veuillez entrer une adresse mail valide.";
         emailError.style.color = "red";
@@ -72,7 +77,7 @@ function validate(event) {
     } else {
         birthdateError.innerHTML = "";
     }
-    //quantity
+    //quantity nombre de tournois entre 0 et 99
     let quantity = document.getElementById("quantity").value;
     let quantityError = document.getElementById("quantityError");
     if (quantity === "" || quantity < 0 || quantity > 99 || isNaN(quantity)) {
@@ -93,7 +98,7 @@ function validate(event) {
         locationError.innerHTML = "";
     }
 
-    //checkbox
+    //checkbox  conditions d'utilisation
     let checkbox = document.getElementById("checkbox1");
     let checkboxError = document.getElementById("checkboxError");
     if (checkbox.checked === false) {
@@ -103,25 +108,19 @@ function validate(event) {
     } else {
         checkboxError.innerHTML = "";
     }
-
+    // si pas d'erreur, message de confirmation
     if (firstNameError.innerHTML === "" && lastNameError.innerHTML === "" && emailError.innerHTML === "" && birthdateError.innerHTML === "" && quantityError.innerHTML === "" && locationError.innerHTML === "" && checkboxError.innerHTML === "") {
         let modal_body = document.getElementsByClassName("modal-body")[0];
-        modal_body.innerHTML = "<p class='success'>Merci ! Votre réservation a été reçue.</p>";
+        modal_body.innerHTML = "<p class='success'>Merci pour votre inscription</p>";
         modal_body.style.textAlign = "center";
-        modal_body.style.fontSize = "1.5rem";
-        modal_body.style.color = "green";
-        modal_body.style.padding = "2rem";
-        modal_body.style.fontWeight = "bold";
-        modal_body.style.backgroundColor = "white";
-        modal_body.style.borderRadius = "10px";
-        modal_body.style.boxShadow = "0 0 10px 0 rgba(0, 0, 0, 0.2)";
-        modal_body.style.margin = "0 auto";
-        modal_body.style.width = "50%";
-        modal_body.style.marginTop = "2rem";
-        modal_body.style.marginBottom = "2rem";
-        modal_body.style.border = "2px solid green";
-        modal_body.style.borderTop = "none";
-        modal_body.style.borderBottom = "none";
+        modal_body.style.fontSize = "36px";
+modal_body.style.width = "100%";
+modal_body.style.height = "100%";
+modal_body.style.padding = "10px";
+modal_body.style.margin = "10px";
+modal_body.style.backgroundColor = "#232323";
+
+
 
         //refresh page if click on close button
         closeBtn.forEach((btn) => btn.addEventListener("click", refreshPage));
